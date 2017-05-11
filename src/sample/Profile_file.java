b2eddb6b5d6f1dc16b83c06881a41237e1584fe1
Kent v. Madsen <Fracturer@outlook.com>
2017-05-11 11:45:50 +0200

    trying again

---
+++ src/sample/Profile_file.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.io.Serializable;
+
+/**
+ * Created by madsen on 5/11/17.
+ */
+public class Profile_file implements Serializable
+{
+    public int Identity;
+    public ProfileType type;
+
+    public String name;
+    public String address;
+    public String phone;
+    public String email;
+
+    public String password;
+    public String username;
+}
