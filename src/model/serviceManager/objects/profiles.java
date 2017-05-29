2c5554f819a6c9fff8110ed79420b8278cac9e28
Kent <Fracturer@outlook.com>
2017-05-30 01:26:06 +0200

    up

---
+++ src/model/serviceManager/objects/profiles.java
@@ -0,0 +1,14 @@
+package model.serviceManager.objects;
+
+/**
+ * Created by Kentv on 29-05-2017.
+ */
+public class profiles
+{
+    String username, password;
+    
+    public profiles()
+    {
+
+    }
+}
