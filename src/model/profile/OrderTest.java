652428eb34d1424ead274dc4930e2c3aef7d4655
SamEmam <samhoffmann111@hotmail.com>
2017-05-30 00:51:22 +0200

    OrderTest

---
+++ src/model/profile/OrderTest.java
@@ -0,0 +1,56 @@
+package model.profile;
+
+import com.sun.deploy.ui.ProgressDialog;
+import model.product.Product;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sam on 23-05-2017.
+ */
+public class OrderTest {
+
+    @Test
+    public void validOrder() throws Exception {
+        try {
+            new Order(new HashMap<Product,double>(),42,42.42);
+            fail("Creating an invalid Order throws IllegalArgumentException");
+        }catch (IllegalArgumentException e) {
+        }
+    }
+
+
+    @Test
+    public void getProductPriceMap() throws Exception {
+
+    }
+
+    @Test
+    public void getOrderNumber() throws Exception {
+
+    }
+
+    @Test
+    public void getPrice() throws Exception {
+
+    }
+
+    @Test
+    public void getDate() throws Exception {
+
+    }
+
+    @Test
+    public void getStatus() throws Exception {
+
+    }
+
+    @Test
+    public void setStatus() throws Exception {
+
+    }
+
+}
